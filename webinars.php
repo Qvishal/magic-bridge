a0aba7f563fc24870fed9002a0a03e41628f0b50
Vishal Mehta <vishal7423@gmail.com>
2025-06-15 02:20:43 +0530

    created webinars page

--- webinars.php
+++ webinars.php
@@ -1,2 +1,226 @@
 <?php include "includes/header.php" ?>
+<section class="px-4 lg:px-0 container mx-auto mt-8 lg:mt-16">
+    <div class="text-center mb-10">
+        <span class="text-lg lg:text-base font-semibold text-secondary block mb-2">Webinars</span>
+        <h1 class="text-[40px] leading-[50px] font-semibold"><span class="text-accent">Learn</span> More, <span class="text-accent">Earn</span> More</h1>
+    </div>
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div class="flex flex-col border border-[#6565C7] p-4 rounded gap-6 justify-center lg:items-start">
+            <h2 class="text-[24px] leading-[32px] font-semibold w-[80%]">The Future of Blockchain and Cloud Integration</h2>
+            <div class="grid grid-cols-3 w-full lg:w-[70%]">
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>                                
+            </div>
+            <div class="flex flex-col lg:flex-row justify-center gap-[20px] lg:gap-[30px] lg:items-center">
+                <button class="bg-[#426BFF] text-white px-4 py-2 rounded-md w-fit">Book Your Seat</button>
+                <div class="flex gap-[40px]">
+                    <span class="text-base">26 Jun 2025</span>
+                    <span class="text-base">3:00 PM IST</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col border border-[#6565C7] p-4 rounded gap-6 justify-center lg:items-start">
+            <h2 class="text-[24px] leading-[32px] font-semibold w-[80%]">The Future of Blockchain and Cloud Integration</h2>
+            <div class="grid grid-cols-3 w-full lg:w-[70%]">
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>                                
+            </div>
+            <div class="flex flex-col lg:flex-row justify-center gap-[20px] lg:gap-[30px] lg:items-center">
+                <button class="bg-[#426BFF] text-white px-4 py-2 rounded-md w-fit">Book Your Seat</button>
+                <div class="flex gap-[40px]">
+                    <span class="text-base">26 Jun 2025</span>
+                    <span class="text-base">3:00 PM IST</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col border border-[#6565C7] p-4 rounded gap-6 justify-center lg:items-start">
+            <h2 class="text-[24px] leading-[32px] font-semibold w-[80%]">The Future of Blockchain and Cloud Integration</h2>
+            <div class="grid grid-cols-3 w-full lg:w-[70%]">
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>                                
+            </div>
+            <div class="flex flex-col lg:flex-row justify-center gap-[20px] lg:gap-[30px] lg:items-center">
+                <button class="bg-[#426BFF] text-white px-4 py-2 rounded-md w-fit">Book Your Seat</button>
+                <div class="flex gap-[40px]">
+                    <span class="text-base">26 Jun 2025</span>
+                    <span class="text-base">3:00 PM IST</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col border border-[#6565C7] p-4 rounded gap-6 justify-center lg:items-start">
+            <h2 class="text-[24px] leading-[32px] font-semibold w-[80%]">The Future of Blockchain and Cloud Integration</h2>
+            <div class="grid grid-cols-3 w-full lg:w-[70%]">
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>                                
+            </div>
+            <div class="flex flex-col lg:flex-row justify-center gap-[20px] lg:gap-[30px] lg:items-center">
+                <button class="bg-[#426BFF] text-white px-4 py-2 rounded-md w-fit">Book Your Seat</button>
+                <div class="flex gap-[40px]">
+                    <span class="text-base">26 Jun 2025</span>
+                    <span class="text-base">3:00 PM IST</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col border border-[#6565C7] p-4 rounded gap-6 justify-center lg:items-start">
+            <h2 class="text-[24px] leading-[32px] font-semibold w-[80%]">The Future of Blockchain and Cloud Integration</h2>
+            <div class="grid grid-cols-3 w-full lg:w-[70%]">
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>                                
+            </div>
+            <div class="flex flex-col lg:flex-row justify-center gap-[20px] lg:gap-[30px] lg:items-center">
+                <button class="bg-[#426BFF] text-white px-4 py-2 rounded-md w-fit">Book Your Seat</button>
+                <div class="flex gap-[40px]">
+                    <span class="text-base">26 Jun 2025</span>
+                    <span class="text-base">3:00 PM IST</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col border border-[#6565C7] p-4 rounded gap-6 justify-center lg:items-start">
+            <h2 class="text-[24px] leading-[32px] font-semibold w-[80%]">The Future of Blockchain and Cloud Integration</h2>
+            <div class="grid grid-cols-3 w-full lg:w-[70%]">
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>                                
+            </div>
+            <div class="flex flex-col lg:flex-row justify-center gap-[20px] lg:gap-[30px] lg:items-center">
+                <button class="bg-[#426BFF] text-white px-4 py-2 rounded-md w-fit">Book Your Seat</button>
+                <div class="flex gap-[40px]">
+                    <span class="text-base">26 Jun 2025</span>
+                    <span class="text-base">3:00 PM IST</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col border border-[#6565C7] p-4 rounded gap-6 justify-center lg:items-start">
+            <h2 class="text-[24px] leading-[32px] font-semibold w-[80%]">The Future of Blockchain and Cloud Integration</h2>
+            <div class="grid grid-cols-3 w-full lg:w-[70%]">
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>                                
+            </div>
+            <div class="flex flex-col lg:flex-row justify-center gap-[20px] lg:gap-[30px] lg:items-center">
+                <button class="bg-[#426BFF] text-white px-4 py-2 rounded-md w-fit">Book Your Seat</button>
+                <div class="flex gap-[40px]">
+                    <span class="text-base">26 Jun 2025</span>
+                    <span class="text-base">3:00 PM IST</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col border border-[#6565C7] p-4 rounded gap-6 justify-center lg:items-start">
+            <h2 class="text-[24px] leading-[32px] font-semibold w-[80%]">The Future of Blockchain and Cloud Integration</h2>
+            <div class="grid grid-cols-3 w-full lg:w-[70%]">
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <img src="assets/images/webinar.png" alt="" class="h-[40px] w-[40px]">
+                    <p class="text-sm font-semibold">Name</p>
+                    <p class="text-sm">Designation Company</p>
+                </div>                                
+            </div>
+            <div class="flex flex-col lg:flex-row justify-center gap-[20px] lg:gap-[30px] lg:items-center">
+                <button class="bg-[#426BFF] text-white px-4 py-2 rounded-md w-fit">Book Your Seat</button>
+                <div class="flex gap-[40px]">
+                    <span class="text-base">26 Jun 2025</span>
+                    <span class="text-base">3:00 PM IST</span>
+                </div>
+            </div>
+        </div>        
+    </div>
+</section>    
 <?php include "includes/footer.php" ?>
